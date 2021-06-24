20716f2945eaad0e80f31b3779c8af093ac13f39
AlexDev39 <alexhavilla@gmail.com>
2021-06-24 18:43:56 -0300

    Projeto Pay Flow criado

---
+++ android/app/src/main/kotlin/com/example/palflow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.palflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modules/login/login_controller.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:palflow/modules/models/user_model.dart';
+import 'package:palflow/shared/auth/auth_controller.dart';
+
+class LoginController {
+  final authController = AuthController();
+  Future<void> googleSingIn(BuildContext context) async {
+    GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: [
+        'email',
+      ],
+    );
+
+    try {
+      final response = await _googleSignIn.signIn();
+      final user = UserModel(
+        name: response!.displayName!,
+        photoURL: response.photoUrl,
+      );
+      authController.setUser(context, user);
+      print(response);
+    } catch (error) {
+      authController.setUser(context, null);
+      print(error);
+    }
+  }
+}
